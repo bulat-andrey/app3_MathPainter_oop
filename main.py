dfcfb170f321a696858e1d069aaa20afeea047d9
Andrei Bulatov <bulatov.a@dbbest.com>
2022-12-26 13:18:10 +0100

    CLI done im main.py

--- main.py
+++ main.py
@@ -1,9 +1,47 @@
 from canvas import Canvas
 from shapes import Rectangle, Square
 
-canvas = Canvas(height=20, width=30, color=(255, 255, 255))
-r1 = Rectangle(x=1, y=6, height=7, width=10, color=(100, 200, 255))
-r1.draw(canvas)
-s1 = Square(x=1, y=3, side=3, color=(0, 100, 222))
-s1.draw(canvas)
+canvas_width = int(input("Enter canvas width: "))
+canvas_height = int(input("Enter canvas height: "))
+
+colors = {'white': (255, 255, 255), 'black': (0, 0, 0)}
+canvas_color = input('Enter canvas color (white or black): ')
+
+# create a canvas with user data
+canvas = Canvas(height=canvas_height, width=canvas_width, color=colors[canvas_color])
+
+while True:
+    shape_type = input('What do you like to draw? Enter quit to quit. ')
+
+    # ask for rectangle data and create rectangle if user entered 'rectangle'
+
+    if shape_type.lower() == 'rectangle':
+        rec_x = int(input("Enter x of the rectangle: "))
+        rec_y = int(input("Enter y of the rectangle: "))
+        rec_width = int(input("Enter the width of the rectangle: "))
+        rec_height = int(input("Enter the width of the rectangle: "))
+        red = int(input("How much red should the rectangle have? "))
+        green = int(input("How much green should the rectangle have? "))
+        blue = int(input("How much blue should the rectangle have? "))
+
+        # create the rectangle
+        r1 = Rectangle(x=rec_x, y=rec_y, width=rec_width, height=rec_height, color=(red, green, blue))
+        r1.draw(canvas)
+
+    if shape_type.lower() == 'square':
+        sqr_x = int(input("Enter x of the square: "))
+        sqr_y = int(input("Enter y of the square: "))
+        sqr_side = int(input("Enter the side of the square: "))
+        red = int(input("How much red should the square have? "))
+        green = int(input("How much green should the square have? "))
+        blue = int(input("How much blue should the square have? "))
+        # create the sqr tangle
+        s1 = Square(x=sqr_x, y=sqr_y, side=sqr_side, color=(red, green, blue))
+        s1.draw(canvas)
+
+    # Break the loop:
+    if shape_type.lower() == 'quit':
+        break
+
 canvas.make('canvas.png')
+
