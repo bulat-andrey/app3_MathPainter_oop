9f69c2257b604016c88e4cbd42c42928a46d688f
Andrei Bulatov <bulatov.a@dbbest.com>
2022-12-26 13:00:06 +0100

    refactored

--- main.py
+++ main.py
@@ -1,37 +1,9 @@
-class Canvas:
+from canvas import Canvas
+from shapes import Rectangle, Square
 
-    def __init__(self, a, b, color):
-        self.b = b
-        self.a = a
-        self.color = color
-
-    def make(self):
-        pass
-
-
-class Rectangle:
-
-    def __init__(self, x, y, a, b, color):
-        self.color = color
-        self.b = b
-        self.a = a
-        self.y = y
-        self.x = x
-
-
-    def draw(self, canvas):
-        pass
-
-
-class Square:
-
-    def __init__(self, x, y, a, color):
-        self.color = color
-        self.a = a
-        self.y = y
-        self.x = x
-
-    def draw(self, canvas):
-        pass
-
-
+canvas = Canvas(height=20, width=30, color=(255, 255, 255))
+r1 = Rectangle(x=1, y=6, height=7, width=10, color=(100, 200, 255))
+r1.draw(canvas)
+s1 = Square(x=1, y=3, side=3, color=(0, 100, 222))
+s1.draw(canvas)
+canvas.make('canvas.png')
